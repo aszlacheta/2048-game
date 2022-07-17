692135077b6c9616c91a5f6e369dafbd4ebdcfc1
Aleksandra Szlacheta <aleksandra.szlacheta@gmail.com>
2022-07-17 15:03:20 +0200

    Added support of Polish language

--- src/i18n.js
+++ src/i18n.js
@@ -1,23 +1,29 @@
 import i18n from "i18next";
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import EN from "./i18n/en.json";
+import PL from "./i18n/pl.json";
 
 const DEFAULT_LANGUAGE = 'en';
 const resources = {
   en: {
     translation: EN,
+  },
+  pl: {
+    translation: PL,
   }
 };
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(I18nextBrowserLanguageDetector)
+  .use(initReactI18next)
   .init({
     resources,
-    lng: DEFAULT_LANGUAGE, 
-
+    fallbackLng: DEFAULT_LANGUAGE,
+    supportedLngs: ['en', 'pl'],
     interpolation: {
       escapeValue: false // react already safes from xss
     }
   });
 
-  export default i18n;
+export default i18n;
