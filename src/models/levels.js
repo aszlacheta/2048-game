d7e9c14469de8f6edb645b50da6460646038fed4
Aleksandra Szlacheta <aleksandra.szlacheta@gmail.com>
2022-07-18 10:20:17 +0200

    Add third and fourth level

--- src/models/levels.js
+++ src/models/levels.js
@@ -1,10 +1,14 @@
+import levelFourth from "./levelFourth";
+import levelThird from "./levelThird";
 import levelTwo from "./levelTwo";
 
 const levels = {
     [levelTwo.level]: levelTwo,
+    [levelThird.level]: levelThird,
+    [levelFourth.level]: levelFourth,
 }
 
 /** 
- * Represents models for differen levels of the game
+ * Represents models for different levels of the game
  */
 export default levels;
